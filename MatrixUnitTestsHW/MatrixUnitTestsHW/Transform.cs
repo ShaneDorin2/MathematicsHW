745ca48165eda3c060b32da1cc0ee28a88a8185d
ShaneDorin <shane.dorin@edu.devinci.fr>
2024-12-26 19:12:06 +0100

    WIP exo 18

--- MatrixUnitTestsHW/MatrixUnitTestsHW/Transform.cs
+++ MatrixUnitTestsHW/MatrixUnitTestsHW/Transform.cs
@@ -4,16 +4,18 @@ using System.Numerics;
 
 namespace Maths_Matrices.Tests
 {
-    // Exercices 17 ----------------------------------------------------------------------------------------
 
     public class Transform
     {
+        // Exercices 17 ----------------------------------------------------------------------------------------
+
         public Transform() { }
 
-        public Vector3 LocalPosition = new Vector3(0f,0f,0f);
+        public Vector3 LocalPosition = new Vector3(0f, 0f, 0f);
 
-        public MatrixFloat LocalTranslationMatrix { 
-            get{ return GenerateLocalTranslationMatrix(); } 
+        public MatrixFloat LocalTranslationMatrix
+        {
+            get { return GenerateLocalTranslationMatrix(); }
         }
 
         private MatrixFloat GenerateLocalTranslationMatrix()
@@ -22,6 +24,40 @@ namespace Maths_Matrices.Tests
             SolMat[0, 3] = LocalPosition.X;
             SolMat[1, 3] = LocalPosition.Y;
             SolMat[2, 3] = LocalPosition.Z;
+            return SolMat;
+        }
+
+
+        // Exercices 18 ----------------------------------------------------------------------------------------
+
+        public Vector3 LocalRotation = new Vector3(0f, 0f, 0f);
+
+        public MatrixFloat LocalRotationMatrix
+        {
+            get { return GenerateLocalRotationXMatrix(); }
+        }
+
+        public MatrixFloat LocalRotationXMatrix
+        {
+            get { return GenerateLocalTranslationMatrix(); }
+        }
+        public MatrixFloat LocalRotationYMatrix
+        {
+            get { return GenerateLocalTranslationMatrix(); }
+        }
+        public MatrixFloat LocalRotationZMatrix
+        {
+            get { return GenerateLocalTranslationMatrix(); }
+        }
+
+
+        private MatrixFloat GenerateLocalRotationXMatrix()
+        {
+            MatrixFloat SolMat = MatrixFloat.Identity(4);
+            SolMat[1, 1] = (float)Math.Cos(LocalRotation.X);
+            SolMat[1, 2] = (float)Math.Sin(LocalRotation.X) *-1;
+            SolMat[2, 1] = (float)Math.Sin(LocalRotation.X);
+            SolMat[2, 2] = (float)Math.Cos(LocalRotation.X);
             return SolMat;
         }
     }
