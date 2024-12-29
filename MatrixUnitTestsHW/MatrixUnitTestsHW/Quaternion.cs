56cb5f4f7285a95204bef42e001b0df097fd255d
ShaneDorin <shane.dorin@edu.devinci.fr>
2024-12-29 17:56:42 +0100

    exo 24

--- MatrixUnitTestsHW/MatrixUnitTestsHW/Quaternion.cs
+++ MatrixUnitTestsHW/MatrixUnitTestsHW/Quaternion.cs
@@ -1,7 +1,12 @@
-﻿namespace Maths_Matrices.Tests
+﻿using System;
+using System.Numerics;
+
+namespace Maths_Matrices.Tests
 {
     public struct Quaternion
     {
+        // Exercices 23 ----------------------------------------------------------------------------------------
+
         public float x, y, z, w;
 
         public Quaternion(float x, float y, float z, float w)
@@ -13,5 +18,21 @@
         }
 
         public static Quaternion Identity => new Quaternion(0, 0, 0, 1);
+
+        // Exercices 24 ----------------------------------------------------------------------------------------
+
+        public static Quaternion AngleAxis(float angle, Vector3 axis)
+        {
+
+            float magnitude = (float)Math.Sqrt(axis.X * axis.X + axis.Y * axis.Y + axis.Z * axis.Z);
+            if (magnitude == 0)
+            {
+                throw new ArgumentException("Axis must be non-zero.");
+            }
+            Vector3 normalizedAxis = new Vector3(axis.X / magnitude, axis.Y / magnitude, axis.Z / magnitude);
+
+            System.Numerics.Quaternion solQuat = System.Numerics.Quaternion.CreateFromAxisAngle(normalizedAxis, (float)(angle * Math.PI / 180));
+            return new Quaternion(solQuat.X, solQuat.Y, solQuat.Z, solQuat.W);
+        }
     }
 }
--- MatrixUnitTestsHW/MatrixUnitTestsHW/Tests24_QuaternionsAngleAxis.cs
+++ MatrixUnitTestsHW/MatrixUnitTestsHW/Tests24_QuaternionsAngleAxis.cs
@@ -1,4 +1,6 @@
 using NUnit.Framework;
+using NUnit.Framework.Legacy;
+using System.Numerics;
 
 namespace Maths_Matrices.Tests
 {
@@ -6,6 +8,7 @@ namespace Maths_Matrices.Tests
     public class Tests24_QuaternionsAngleAxis
     {
         [Test]
+        [DefaultFloatingPointTolerance(0.01d)]
         public void TestQuaternionAngleAxisX()
         {
             //GlobalSettings.DefaultFloatingPointTolerance = 0.01d;
@@ -13,16 +16,17 @@ namespace Maths_Matrices.Tests
             //90 degree around X axis
             float angle = 90f;
             Vector3 axis = new Vector3(1f, 0f, 0f);
-            Quaternion q = Quaternion.AngleAxis(angle, axis);
-            Assert.AreEqual(0.71f, q.x);
-            Assert.AreEqual(0f, q.y);
-            Assert.AreEqual(0f, q.z);
-            Assert.AreEqual(0.71f, q.w);
+            Tests.Quaternion q = Tests.Quaternion.AngleAxis(angle, axis);
+            ClassicAssert.AreEqual(0.71f, q.x);
+            ClassicAssert.AreEqual(0f, q.y);
+            ClassicAssert.AreEqual(0f, q.z);
+            ClassicAssert.AreEqual(0.71f, q.w);
 
             //GlobalSettings.DefaultFloatingPointTolerance = 0.0d;
         }
 
         [Test]
+        [DefaultFloatingPointTolerance(0.01d)]
         public void TestQuaternionAngleAxisY()
         {
             //GlobalSettings.DefaultFloatingPointTolerance = 0.01d;
@@ -30,47 +34,49 @@ namespace Maths_Matrices.Tests
             //90 degree around Y axis
             float angle = 90f;
             Vector3 axis = new Vector3(0f, 1f, 0f);
-            Quaternion q = Quaternion.AngleAxis(angle, axis);
-            Assert.AreEqual(0f, q.x);
-            Assert.AreEqual(0.71f, q.y);
-            Assert.AreEqual(0f, q.z);
-            Assert.AreEqual(0.71f, q.w);
+            Tests.Quaternion q = Tests.Quaternion.AngleAxis(angle, axis);
+            ClassicAssert.AreEqual(0f, q.x);
+            ClassicAssert.AreEqual(0.71f, q.y);
+            ClassicAssert.AreEqual(0f, q.z);
+            ClassicAssert.AreEqual(0.71f, q.w);
 
             //GlobalSettings.DefaultFloatingPointTolerance = 0.0d;
         }
 
         [Test]
+        [DefaultFloatingPointTolerance(0.01d)]
         public void TestQuaternionAngleAxisZ()
         {
-            GlobalSettings.DefaultFloatingPointTolerance = 0.01d;
+            //GlobalSettings.DefaultFloatingPointTolerance = 0.01d;
 
             //90 degree around Z axis
             float angle = 90f;
             Vector3 axis = new Vector3(0f, 0f, 1f);
-            Quaternion q = Quaternion.AngleAxis(angle, axis);
-            Assert.AreEqual(0f, q.x);
-            Assert.AreEqual(0f, q.y);
-            Assert.AreEqual(0.71f, q.z);
-            Assert.AreEqual(0.71f, q.w);
+            Tests.Quaternion q = Tests.Quaternion.AngleAxis(angle, axis);
+            ClassicAssert.AreEqual(0f, q.x);
+            ClassicAssert.AreEqual(0f, q.y);
+            ClassicAssert.AreEqual(0.71f, q.z);
+            ClassicAssert.AreEqual(0.71f, q.w);
 
-            GlobalSettings.DefaultFloatingPointTolerance = 0.0d;
+            //GlobalSettings.DefaultFloatingPointTolerance = 0.0d;
         }
 
         [Test]
+        [DefaultFloatingPointTolerance(0.01d)]
         public void TestQuaternionCustomAxis()
         {
-            GlobalSettings.DefaultFloatingPointTolerance = 0.01d;
+            //GlobalSettings.DefaultFloatingPointTolerance = 0.01d;
 
             //60 degree around Vector(0,3,4)
             float angle = 60f;
             Vector3 axis = new Vector3(0f, 3f, 4f);
-            Quaternion q = Quaternion.AngleAxis(angle, axis);
-            Assert.AreEqual(0f, q.x);
-            Assert.AreEqual(0.3f, q.y);
-            Assert.AreEqual(0.4f, q.z);
-            Assert.AreEqual(0.87f, q.w);
+            Tests.Quaternion q = Tests.Quaternion.AngleAxis(angle, axis);
+            ClassicAssert.AreEqual(0f, q.x);   
+            ClassicAssert.AreEqual(0.3f, q.y);
+            ClassicAssert.AreEqual(0.4f, q.z);
+            ClassicAssert.AreEqual(0.87f, q.w);
 
-            GlobalSettings.DefaultFloatingPointTolerance = 0.0d;
+            //GlobalSettings.DefaultFloatingPointTolerance = 0.0d;
         }
     }
 }
